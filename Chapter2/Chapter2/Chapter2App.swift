1d306d7ab1b1b627685e1881826df4207d4359ae
hanhyeongyu <kss1022hhh@naver.com>
2023-07-18 00:16:55 +0900

    Drawing Paths and Shapes

---
+++ Chapter2/Chapter2/Chapter2App.swift
@@ -0,0 +1,17 @@
+//
+//  Chapter2App.swift
+//  Chapter2
+//
+//  Created by 한현규 on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct Chapter2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
